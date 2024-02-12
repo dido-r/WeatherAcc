9a9bb3fddb7f7979900f54ecf3f4a224853c1c32
dido-r <deyan_r@abv.bg>
2024-02-12 18:31:20 +0200

    Home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-  NavbarComponent],
-  template: `<app-navbar></app-navbar>`,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <app-home></app-home>
+    <app-footer></app-footer>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  template: `
+    <footer>WeatherAcc, 2024</footer>
+  `,
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
